41d758b46de87258adcfb52c5b98344ce7711053
Jose Jordan <jordanjose0410@gmail.com>
2021-05-07 20:34:30 -0400

    Codigo comentado

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int digito(int num, int casa_decimal, int base){
+int digito(int num, int casa_decimal, int base){ //Extracao de digitos
     int d;
     if(base == 10){
         d = num/(pow(10, casa_decimal));
@@ -12,23 +12,23 @@ int digito(int num, int casa_decimal, int base){
     return 0;
 }
 
-void radix(int *v, int n, int base, int num_digitos) {
+void radix(int *v, int n, int base, int num_digitos) { //vetor, quantidade de numeros, base decimal, digitos dos numeros
     int i, j, w, count[base+1], d, posicao;
     int *aux = (int *) malloc(n * sizeof(int));
-    for(w = 0; w < num_digitos; w++) {
-        for(j = 0; j < base; j++) count[j] = 0;
-        for(i = 0; i < n; i++) { 
-            d = digito(v[i], w, base);
-            count[d+1]++;
+    for(w = 0; w < num_digitos; w++) { //percorrendo os digitos para ordenar
+        for(j = 0; j < base; j++) count[j] = 0; //contador de numeros baseado em seus digitos
+        for(i = 0; i < n; i++) { //percorrendo vetor e extraindo o digito dos elementos
+            d = digito(v[i], w, base); //extraindo o digito que esta sendo analisado
+            count[d+1]++; //contando quantos numeros vao estar em casa posicao
         }
-        for(j = 1; j < base; j++) count[j] += count[j-1];
-        for(i = 0; i < n; i++) {
-            d = digito(v[i], w, base);
-            posicao = count[d];
-            count[d] += 1;
-            aux[posicao] = v[i];
+        for(j = 1; j < base; j++) count[j] += count[j-1]; //Pegando a posicao de inicio de cada digito
+        for(i = 0; i < n; i++) { //Percorrendo vetor para ordenar
+            d = digito(v[i], w, base); //Digito para definir a posicao no vetor
+            posicao = count[d]; //pegando a posicao de insercao no vetor
+            count[d] += 1; //nova posicao de insercao de determinado digito
+            aux[posicao] = v[i]; //adicionando o elemento na nova posicao
             }
-            for(i = 0; i < n; i++) v[i] = aux[i];
+            for(i = 0; i < n; i++) v[i] = aux[i]; //vetor recebe a nova organizacao
     }
 }
 
